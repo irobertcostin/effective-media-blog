0d78b2713c6626d01a6ea12f076ceaa2ffba3123
irobertcostin <irobertcostin@gmail.com>
2024-04-06 15:03:22 +0300

    app update

---
+++ src/components/home/Home.js
@@ -0,0 +1,24 @@
+import UtilityBar from "./UtilityBar"
+
+
+
+
+
+
+
+export default function Home() {
+
+
+
+    return (
+        <div className="w-full relative py-2 px-2">
+
+
+            <UtilityBar />
+
+
+
+
+        </div>
+    )
+}
