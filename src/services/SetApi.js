c56114df7d54a2c6cb12886067fa8d01d64d9a9a
irobertcostin <irobertcostin@gmail.com>
2024-04-07 23:09:54 +0300

    created blog posts service

---
+++ src/services/SetApi.js
@@ -0,0 +1,14 @@
+
+const getApiUrl = () => {
+
+    if (process.env.NODE_ENV === "development") {
+        return process.env.REACT_APP_API_URL_DEV
+    } else {
+        return process.env.REACT_APP_API_URL_PROD
+    }
+
+
+}
+
+
+export default getApiUrl;
